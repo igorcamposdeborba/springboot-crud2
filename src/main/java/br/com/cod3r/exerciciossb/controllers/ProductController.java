9f4a6aa22a63e3850dbef2478eaf01d9ee9f0d20
igorborba <igor_campos5@hotmail.com>
2023-01-25 19:39:08 -0300

    Delete by id implemented

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
@@ -3,7 +3,9 @@ package br.com.cod3r.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +57,10 @@ public class ProductController {
 		productRepository.save(product);
 		
 		return product;
+	}
+	
+	@DeleteMapping (path = "/delete")
+	public void delete(@RequestParam(name = "id") Integer id) {
+		productRepository.deleteById(id);
 	}
 }
