73763a6a511ad6a8e551e720dd9dcd63c2d20155
igorborba <igor_campos5@hotmail.com>
2023-01-25 18:52:32 -0300

    Find all and by id implemented

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +24,6 @@ public class ProductController {
 	
 	@PostMapping
 	public @ResponseBody Product newProduct(@Valid Product product) { // body da resposta do servidor (Product) e body da requisição. @Valid aceita só objeto com atributos válidos segundo as annotations da classe Product
-		
 		productRepository.save(product); // salvar produto no banco de dados. save é um método do CrudRepository no ProductRepository
 		return product;
 	}
@@ -33,6 +36,16 @@ public class ProductController {
 		productRepository.save(product);
 		
 		return product;
+	}
+	
+	@GetMapping (path = "/find-all")
+	public Iterable<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	@GetMapping (path = "/find-by-id{id}")
+	public Optional<Product> findById(@RequestParam(name = "id") Integer id) {
+		return productRepository.findById(id);
 	}
 	
 }
