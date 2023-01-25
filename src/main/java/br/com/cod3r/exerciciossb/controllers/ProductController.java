f47c2cc8b574e3a4af6933c5fd896bf5663a43a7
igorborba <igor_campos5@hotmail.com>
2023-01-25 20:08:32 -0300

    find all paginated

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.com.cod3r.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +47,16 @@ public class ProductController {
 		return productRepository.findAll();
 	}
 	
+	@GetMapping (path = "/page{pageNumber}{quantityPerPage}")
+	public Iterable<Product> getProductByPage(@RequestParam(value = "pageNumber") int pageNumber, @RequestParam(value = "quantityPerPage") int quantityPerPage){
+		if (quantityPerPage > 100) {
+			quantityPerPage = 5;
+		}
+		
+		Pageable page = PageRequest.of(pageNumber, quantityPerPage);
+		return productRepository.findAll(page);
+	}
+	
 	@GetMapping (path = "/find-by-id{id}")
 	public Optional<Product> findById(@RequestParam(name = "id") Integer id) {
 		return productRepository.findById(id);
@@ -63,4 +75,6 @@ public class ProductController {
 	public void delete(@RequestParam(name = "id") Integer id) {
 		productRepository.deleteById(id);
 	}
+	
+	
 }
--- src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProductRepository.java
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProductRepository.java
@@ -1,10 +1,11 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.cod3r.exerciciossb.model.entities.Product;
 
 // interface repository abstrai o acesso aos dados por meio de injeção de dependência para que eu possa acessar os métodos de um CRUD
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends CrudRepository<Product, Integer>, PagingAndSortingRepository <Product, Integer>{
 	
 }
